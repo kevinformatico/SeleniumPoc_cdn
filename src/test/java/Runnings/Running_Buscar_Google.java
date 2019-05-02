8ab7213c81f19c4d41166e1517a24420126c168a
Kevin Ferreyra <kevin.f.e@hotmail.com>
2019-05-02 11:22:48 -0400

    Agregar reporte Junit

--- src/test/java/Runnings/Running_Buscar_Google.java
+++ src/test/java/Runnings/Running_Buscar_Google.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(tags = "@buscargoogle")
+@CucumberOptions(tags = "@buscargoogle", plugin = {"junit:target/cucumber.xml"})
 
 public class Running_Buscar_Google extends Running{
 }
