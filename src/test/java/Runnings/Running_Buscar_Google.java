6fad57c3fb4e4e114fa224e5b8b6752fd1e22fb8
Kevin Ferreyra <kevin.f.e@hotmail.com>
2019-05-09 10:37:40 -0400

    prueba

--- src/test/java/Runnings/Running_Buscar_Google.java
+++ src/test/java/Runnings/Running_Buscar_Google.java
@@ -8,5 +8,4 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"junit:target/cucumber.xml", "json:target/cucumber.json"})
 
-public class Running_Buscar_Google extends Running{
-}
+public class Running_Buscar_Google extends Runnin
